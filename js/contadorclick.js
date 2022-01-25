a45c3765e1137fbeb2078c10a3d440d8266e9390
colorodriguezf <colitorodriguez97@gmail.com>
2022-01-25 16:57:35 -0300

    falta agregarle sonido al boton reset, y cambiar letras

--- js/contadorclick.js
+++ js/contadorclick.js
@@ -1,12 +1,36 @@
 "use strict"; 
-function cargarPagina () {
-let contador=0;
 
-function contar () {
-    contador++;
-    document.getElementById("contarCLicks").innerHTML= contador;
-}
-document.getElementById("btn-contar").addEventListener("click", contar);
+let contador = document.getElementById("contarCLicks");
+document.getElementById("sumar1").addEventListener("click", sumar1);
+document.getElementById("restar1").addEventListener("click", restar1);
+document.getElementById("reiniciar").addEventListener("click",reiniciar);
 
+let numero= 0;
+
+function sumar1() {
+    numero++;
+    contador.innerHTML=numero;
 }
-document.addEventListener("DOMContentLoaded", cargarPagina);
+const audioSumar = new Audio();
+audioSumar.src= "sound-effects/ES_Bubble Blip 2 - SFX Producer.mp3";
+
+function restar1() {
+    if (numero>0) {
+        numero--;
+        contador.innerHTML=numero;
+    }
+    else {
+        document.getElementById("error").innerHTML="El contador no puede ser negativo";
+        setTimeout(error,2000);
+    }
+}
+const audioRestar = new Audio();
+audioRestar.src="sound-effects/ES_Suction Pop 3 - SFX Producer (mp3cut.net).mp3";
+function error() {
+        document.getElementById("error").innerHTML="";
+    }
+function reiniciar() {
+        numero=0;
+        contador.innerHTML=numero;
+    }
+
